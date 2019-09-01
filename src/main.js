79a1bf1ac3c261ab47e8deb293473344fbd9cf05
fff <rkhalbaginov@mail.ru>
2019-09-01 23:08:15 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vSelect from 'vue-select'
 
-Vue.config.productionTip = false
+Vue.component('v-select', vSelect);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
